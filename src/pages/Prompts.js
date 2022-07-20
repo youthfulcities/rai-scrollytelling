1029363b40338b3e83a01c172434957c4ef3e602
astridcha1x <mixaguilar@yahoo.com>
2022-07-20 04:16:13 -0700

    add Components and Pages based on a few Trello tasks

---
+++ src/pages/Prompts.js
@@ -0,0 +1,7 @@
+/* This Page is for user starting points based on the user story below!
++ REQUIRES: prompts drop down/UI, chosen prompts related to affordability
+
+USER STORY:
+As a youth, I want prompts surrounding affordability so that I have a starting point for sharing my thoughts.
+
+*/
