d7a6f1095caff237bb02a838f50da9a484711616
gennaweber <gennamweber@gmail.com>
2022-08-08 17:33:13 -0400

    Fix picslist

--- src/PicsList.js
+++ src/PicsList.js
@@ -3,118 +3,100 @@ pics from: https://www.pexels.com/search/people/
 
 */
 
-export default picsList = [ // not defined ?
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    {
-      src:
-        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    {
-      src:
-        "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    null,
-    null,
-    null,
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/712521/pexels-photo-712521.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    {
-      src:
-        "https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-  
-    {
-      src:
-        "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/1082962/pexels-photo-1082962.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    {
-      src:
-        "https://images.pexels.com/photos/371160/pexels-photo-371160.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    null,
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    {
-      src:
-        "https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/936313/pexels-photo-936313.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null,
-    {
-      src:
-        "https://images.pexels.com/photos/818819/pexels-photo-818819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    null
-  ];
-  
+const picsList = [
+  null,
+  {
+    src: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    src: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  null,
+  {
+    src: 'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  null,
+  {
+    src: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    src: 'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  null,
+  null,
+  null,
+  null,
+  {
+    src: 'https://images.pexels.com/photos/712521/pexels-photo-712521.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    src: 'https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  {
+    src: 'https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  {
+    src: 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  {
+    src: 'https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+
+  {
+    src: 'https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  {
+    src: 'https://images.pexels.com/photos/1082962/pexels-photo-1082962.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    src: 'https://images.pexels.com/photos/371160/pexels-photo-371160.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  {
+    src: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  null,
+  null,
+  {
+    src: 'https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  {
+    src: 'https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  {
+    src: 'https://images.pexels.com/photos/936313/pexels-photo-936313.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  null,
+  {
+    src: 'https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+  {
+    src: 'https://images.pexels.com/photos/818819/pexels-photo-818819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  null,
+];
+
+export default picsList;
