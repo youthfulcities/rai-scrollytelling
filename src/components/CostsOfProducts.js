2be28fb29c93e61c6cee8ca7aac0763343c17a09
astridcha1x <mixaguilar@yahoo.com>
2022-08-22 13:21:32 -0700

    Remove old and unnecessary component files

--- src/components/CostsOfProducts.js
+++
@@ -1,6 +0,0 @@
-/* This Component is a placeholder based on the Figma Wireframes as of now!
-It is intended for the "Cost of Products" portion of the page
-
-IMG + DESCRIPTIONS + COSTS
-
-Make Horizontal -> can click through, swipe, scroll */
--- src/components/DiplomaJobSearch.js
+++
@@ -1,4 +0,0 @@
-/* This Component is a placeholder based on the Figma Wireframes as of now!
-It is intended for the "Just Graduated?" portion of the page
-
-Mock Diploma -> Animorph into Job Search screen (keep same rectangle size) */
